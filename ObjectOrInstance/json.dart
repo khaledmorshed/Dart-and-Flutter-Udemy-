c8f0b4fce502f005e7cf407ab4a70df14a63e9ff
khaledmorshed <s.m.morshed10@gmail.com>
2022-01-28 11:44:19 +0600

    json with Model class to sepeate data

--- ObjectOrInstance/json.dart
+++ ObjectOrInstance/json.dart
@@ -6,7 +6,17 @@ void main() {
 
   // parseJson json object is a map data structure that has keys and valu
   // dart doesn't know what are the different of types have key and value inside of parsJson.
-  var parseJson = json.decode(rawJson);
-  print(parseJson['url']);
+  var parseJson = json.decode(rawJson); // here are presemt all data with map
+  //print(parseJson['url']);
+  var imageModel = new ImageModel(parseJson["id"], parseJson["url"]);
+  print(imageModel.id);
+  print(imageModel.url);
 }
 
+// with imageModel class here is seperated all data of json
+class ImageModel {
+  int? id;
+  String? url;
+
+  ImageModel(this.id, this.url);
+}
