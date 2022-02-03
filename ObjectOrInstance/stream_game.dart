6db17803d34906356102c70530d1b9fe22682a54
khaledmorshed <s.m.morshed10@gmail.com>
2022-02-03 18:41:04 +0600

    stream with timeout function by guess word .dart

---
+++ ObjectOrInstance/stream_game.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+void main() {
+  final ButtonElement? button = querySelector('button') as ButtonElement?;
+
+  button!.onClick.timeout(
+    new Duration(seconds: 1),
+    onTimeout: (sink)=>sink.addError("you lost!!")
+  ).listen((event) {},
+    onError: (err)=>print(err)  
+  );
+}
+ 
