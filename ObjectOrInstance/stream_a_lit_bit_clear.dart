eba67a93cb3bd662aecd22cef3a5e26cec076ccc
khaledmorshed <s.m.morshed10@gmail.com>
2022-02-03 17:28:47 +0600

    more clear stream with getter

---
+++ ObjectOrInstance/stream_a_lit_bit_clear.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+void main() {
+  final bloc = new Bloc();
+  // bloc.emailController.stream.listen((value) {
+  //   print(value);
+  // });
+  bloc.email.listen((value) {
+    print(value);
+  });
+
+  //(i)
+  //bloc.emailController.sink.add("my new eamil");
+  //instead of (i)
+  bloc.changeEmail("my new email");
+}
+
+class Bloc {
+  final emailController = StreamController<String>();
+  final passwordController = StreamController<String>();
+
+  //get is the shortened form of getter
+  // get changeEmail {
+  //   return emailController.sink.add;
+  // }
+
+  //it gives data to the stream. means change or add data.
+  Function(String) get changeEmail => emailController.sink.add;//Function(String) = optional
+  Function(String) get changePassword => passwordController.sink.add;
+
+  //it retrieves or regains data from the stream email
+  Stream<String> get email => emailController.stream;// Stream<String> = optional
+  get password => passwordController.stream;
+}
