2f664cb2f7b8d485bdb3dddd82719c6ebc3b9fbb
khaledmorshed <s.m.morshed10@gmail.com>
2022-02-09 18:46:35 +0600

    NuclearPlant, PowerGrid, SoalrPlant, abstract PowerPlant = 2

--- ObjectOrInstance/nuclear_power_plant.dart
+++ ObjectOrInstance/nuclear_power_plant.dart
@@ -1,21 +1,35 @@
 void main() {
   PowerGrid grid = PowerGrid();
   NuclearPlant nuclear = NuclearPlant();
+  SolarPlant solar = SolarPlant();
 
   grid.addPlant(nuclear);
+  grid.addPlant(solar);
 }
 
 class PowerGrid {
-  List<NuclearPlant> connectedPlants = [];
+  List<PowerPlant> connectedPlants = [];
 
-  addPlant(NuclearPlant plant) {
+  addPlant(PowerPlant plant) {
     plant.turnOn();
     connectedPlants.add(plant);
   }
 }
 
-class NuclearPlant {
+//So now that our NuclearPlant and SolarPlant qualify as PowerPlant.
+abstract class PowerPlant {
+  turnOn();
+}
+
+//Now NuclearPlant can act as a PowerPlant
+class NuclearPlant implements PowerPlant {
   turnOn() {
     print("I am a nuclear plant turning on");
+  }
+}
+
+class SolarPlant implements PowerPlant {
+  turnOn() {
+    print("I am a solar plant turnig on");
   }
 }
