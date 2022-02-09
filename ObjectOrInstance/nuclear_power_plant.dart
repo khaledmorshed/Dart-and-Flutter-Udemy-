f42ae40a95df9bd1f7122820458294548d2957db
khaledmorshed <s.m.morshed10@gmail.com>
2022-02-09 22:34:00 +0600

    NuclearPlant, PowerGrid, SoalrPlant, abstract PowerPlant and abstract ABuilding  = 3

--- ObjectOrInstance/nuclear_power_plant.dart
+++ ObjectOrInstance/nuclear_power_plant.dart
@@ -11,25 +11,45 @@ class PowerGrid {
   List<PowerPlant> connectedPlants = [];
 
   addPlant(PowerPlant plant) {
-    plant.turnOn();
+    //turnOn method accept a string and return a boolean
+    bool confirmation = plant.turnOn("5 hours");
     connectedPlants.add(plant);
   }
 }
 
 //So now that our NuclearPlant and SolarPlant qualify as PowerPlant.
 abstract class PowerPlant {
-  turnOn();
+  int? costOfEnergy;
+  bool turnOn(String duration);
+}
+
+abstract class Abuilding {
+  int? height;
 }
 
 //Now NuclearPlant can act as a PowerPlant
 class NuclearPlant implements PowerPlant {
-  turnOn() {
+  //it comes from PowePlant instance variable
+  int? costOfEnergy;
+  bool turnOn(String tiemToStayOn) {
     print("I am a nuclear plant turning on");
+    return true;
+  }
+
+  //just make sure that we can add so many method inside of this class
+  //and PowerPlant doesn't have any issue.This class is independet.
+  meltDown() {
+    print("Blows up");
   }
 }
 
-class SolarPlant implements PowerPlant {
-  turnOn() {
+//so A single class can implements multiple abstract classes. 
+class SolarPlant implements PowerPlant, Abuilding {
+  int? costOfEnergy;
+  int? height;
+
+  bool turnOn(String timeToStayOn) {
     print("I am a solar plant turnig on");
+    return false;
   }
 }
