7977abd2ee3825dde0f340f5cc35a6e61eccb3c0
khaledmorshed <s.m.morshed10@gmail.com>
2022-02-11 09:43:58 +0600

    ListView and FutureBuilder working precedure = 1

---
+++ Concept ListView And FutureBuilder/listview/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:listview/screens/list_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "ListView and FutureBuilder concept",
+    home: HomeScreen(),
+  ));
+}
---
+++ Concept ListView And FutureBuilder/listview/lib/screens/list_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Top News"),
+      ),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+    //ListView builder only attemp to render rows that are actually visible on the screen.means it works
+    //with exactly that data which are showing on the screen right now.
+    return ListView.builder(
+      itemCount: 1000,
+      //when we fetch id from repository one by one. itemBuilder build then one bye one. which id will
+      //come first then that id will build first
+      itemBuilder: (context, int index) {
+        //FutureBuilder works as like as Stream
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? Text("I am visible $index")
+                : Text("I havent fetched data yet $index");
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      ()=> 'hi',
+    );
+  }
+}
